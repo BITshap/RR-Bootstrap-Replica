2ab1c138480b57df8a918680f16c8f1e619b54d3
Nick Shapoff <nicholasshapoff@gmail.com>
2022-11-13 10:34:33 -0800

    replica

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import NavBar from './components/NavBar'
+import ImgOverlayExample from './components/SearchBar'
+import GridLayout from './components/GridLayout'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <NavBar />
+      <ImgOverlayExample />
+      <GridLayout />
     </div>
   );
 }
